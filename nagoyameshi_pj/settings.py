71402b98977d9f5853af196c8f55208388f81bec
github-user-am <morioka0729@gmail.com>
2024-04-20 22:08:27 +0900

    change_database

--- nagoyameshi_pj/settings.py
+++ nagoyameshi_pj/settings.py
@@ -80,12 +80,19 @@ WSGI_APPLICATION = 'nagoyameshi_pj.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+import dj_database_url
+    db_from_env = dj_database_url.config()
+    DATABASES = {
+        'default': dj_database_url.config()
     }
-}
+    ALLOWED_HOSTS = ['*']
 
 # import dj_database_url
 # db_from_env = dj_database_url.config()
