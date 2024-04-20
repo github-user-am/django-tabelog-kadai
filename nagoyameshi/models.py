edb8464f9e99d11a89d2efef12fca62c5d3536e0
github-user-am <morioka0729@gmail.com>
2024-04-21 00:30:35 +0900

    modify_createuser

--- nagoyameshi/models.py
+++ nagoyameshi/models.py
@@ -29,23 +29,23 @@ class MyUserManager(BaseUserManager):
 
     use_in_migrations = True
 
-    def _create_user(self, email, nickname, password, **extra_fields):
+    def _create_user(self, email, password, **extra_fields):
         if not email:
             raise ValueError('Users must have an email address')
         email = self.normalize_email(email)
         
-        user = self.model(email=email, nickname=nickname, **extra_fields)
+        user = self.model(email=email, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
     
-    def create_user(self, email, nickname, password=None, **extra_fields):
+    def create_user(self, email, password=None, **extra_fields):
         extra_fields.setdefault('is_admin', False)
         extra_fields.setdefault('is_staff', False)
         extra_fields.setdefault('is_superuser', False)
-        return self._create_user(email, nickname, password, **extra_fields)
+        return self._create_user(email, password, **extra_fields)
     
-    def create_superuser(self, email, nickname, password, **extra_fields):
+    def create_superuser(self, email, password, **extra_fields):
         extra_fields.setdefault('is_admin', True)
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
@@ -57,7 +57,7 @@ class MyUserManager(BaseUserManager):
         if extra_fields.get('is_superuser') is not True:
             raise ValueError('Superuser must have is_superuser=True.')
   
-        return self._create_user(email, nickname, password, **extra_fields)
+        return self._create_user(email, password, **extra_fields)
 
 # User Model
 class CustomUser(AbstractBaseUser, PermissionsMixin):
